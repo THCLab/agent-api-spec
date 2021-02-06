cd9773e5662692e36ffcc162ddb8afaa19928da9
Krzosa <krzosa.karol@gmail.com>
2021-02-06 11:22:24 +0100

    Add post processing python script

--- generate.py
+++ generate.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 import os
 import subprocess
+import sys
 
 path_to_script = os.path.realpath(__file__)
 path = os.path.split(path_to_script) 
 path_to_spec = path[0] + "/swagger-spec.yaml"
 completed = subprocess.run(["swagger-marshmallow-codegen", path_to_spec], capture_output=True)
-print(completed.stdout)
+generated_output = completed.stdout.decode("utf-8")
+generated_output = generated_output.replace("from __future__ import annotations\n", "")
 
-print(path[0])
+generated_filename = "generated.py"
+if len(sys.argv) > 1:
+    generated_filename = sys.argv[1] + generated_filename    
+
+f = open(generated_filename, "w")
+f.write(generated_output)
+f.close()
