f2478214e259a7ff085ea1b1fd4c278907135791
Krzosa <krzosa.karol@gmail.com>
2021-02-10 14:02:47 +0100

    Update scripts, watcher watches both the script and swagger_spec, errors are displayer properly

--- generate.py
+++ generate.py
@@ -12,10 +12,13 @@ import sys
 path_to_script = os.path.realpath(__file__)
 path = os.path.split(path_to_script) 
 path_to_spec = path[0] + "/swagger-spec.yaml"
-completed = subprocess.run(["swagger-marshmallow-codegen", path_to_spec], capture_output=True)
+completed = subprocess.run(["swagger-marshmallow-codegen",  path_to_spec], capture_output=True)
 
 # Format
 generated_output = completed.stdout.decode("utf-8")
+std_error = completed.stderr.decode('utf-8')
+print(std_error)
+
 index = generated_output.find("class")
 generated_output = generated_output[index:]
 generated_output = generated_output.replace("    class Meta:\n        unknown = INCLUDE", "")
@@ -30,6 +33,7 @@ f = open(generated_filename, "w")
 f.write( \
 """\"\"\"This file is auto generated\"\"\"
 from marshmallow import ( Schema, fields, EXCLUDE )
+from marshmallow.validate import OneOf
 
 class OpenAPISchema(Schema):
     class Meta:
--- watch_for_changes.py
+++ watch_for_changes.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import pathlib, time, subprocess, os
 # Settings
 send_generated_model_to_directory = "/home/sevni/Documents/thclab/aries/aries-cloudagent-python/aries_cloudagent/"
@@ -9,15 +10,18 @@ path_to_spec = path[0] + "/swagger-spec.yaml"
 path_to_generate_script = path[0] + "/generate.py"
 
 file = pathlib.Path(path_to_spec)
+generate_script = pathlib.Path(path_to_generate_script)
 last_mod_time = file.stat().st_mtime
+last_mod_time_generate = generate_script.stat().st_mtime
 print(last_mod_time)
 while 1:
     time.sleep(1)
     mod_time = file.stat().st_mtime
-    if last_mod_time != mod_time:
+    mod_time_generate = generate_script.stat().st_mtime
+    if last_mod_time != mod_time or last_mod_time_generate != mod_time_generate:
         last_mod_time = mod_time
-        print("Generate! ...")
+        last_mod_time_generate = mod_time_generate
         out = subprocess.run(["python", path_to_generate_script, send_generated_model_to_directory], capture_output=True)
-        print(out)
-        print("... Done")
+        print("STDOUT: ", out.stdout.decode("utf-8"))
+        print("STDERROR: ", out.stderr.decode("utf-8"))
     
