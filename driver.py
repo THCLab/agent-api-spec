21413636ff8b71489b78eefb669ad72e989d312e
Krzosa <krzosa.karol@gmail.com>
2021-02-11 13:12:31 +0100

    Gitignore

--- driver.py
+++
@@ -1,5 +0,0 @@
-from swagger_marshmallow_codegen.driver import Driver
-
-
-class MyDriver(Driver):
-    codegen_factory = Driver.codegen.override(schema_class_path="foo.bar.schema:MySchema"))
--- test.py
+++
@@ -1,6 +0,0 @@
-import yaml
-
-f = open("swagger-spec.yaml", "r")
-content = f.read()
-content = yaml.safe_load(content)
-print(content['paths'].keys())
